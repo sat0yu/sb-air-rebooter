3ff0882f98d8ebe597a9ec8b1b9e5ae3f33c439f
Yusuke Sato <yusuke1.sato9.git@gmail.com>
2019-08-18 17:59:41 +0900

    reboot softbank air with puppetter

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless: true});
   const page = await browser.newPage();
+
+  // Access the control panel
   await page.goto('http://172.16.255.254');
-  await page.screenshot({path: 'example.png'});
+
+  // Open the authentication modal window
+  await page.click('a[href="javascript:btn_reboot_on();"]');
+
+  // Fill credentials
+  await page.type('#popinAuth #popinBox input#username', 'user');
+  await page.type('#popinAuth #popinBox input#password', 'user');
+
+  // Set an event handler for the confirmation dialog
+  page.on('dialog', async dialog => {
+    console.log(dialog.message());
+    await dialog.accept();
+  });
+
+  // Proceed the authentication (then, open the dialog)
+  await page.click('#popinAuth #popinBox input#login');
+
+  // Collect evidences
+  await page.waitForNavigation({timeout: 10000}),
+  await page.screenshot({path: 'result.png', fullPage: true});
 
   await browser.close();
 })();
